be9080bd84c39d2da2cd0dee0f91166891f9f453
Ian Boyles <ianboyles001@gmail.com>
2023-08-02 19:08:16 +0100

    added statements

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -6,6 +6,22 @@ INSERT INTO book (book_id,book_name) VALUES (3,'Trading_book_3');
 INSERT INTO book (book_id,book_name) VALUES (4,'Trading_book_4');
 INSERT INTO book (book_id,book_name) VALUES (5,'Trading_book_6');
 
+INSERT INTO book_user ()
+
+INSERT INTO security (security_id,cusip,isin,issuer_name, bond_maturity_date,bond_currency,face_value) VALUES ();
+INSERT INTO security (security_id,cusip,isin,issuer_name, bond_maturity_date,bond_currency,face_value) VALUES ();
+INSERT INTO security (security_id,cusip,isin,issuer_name, bond_maturity_date,bond_currency,face_value) VALUES ();
+INSERT INTO security (security_id,cusip,isin,issuer_name, bond_maturity_date,bond_currency,face_value) VALUES ();
+INSERT INTO security (security_id,cusip,isin,issuer_name, bond_maturity_date,bond_currency,face_value) VALUES ();
+INSERT INTO security (security_id,cusip,isin,issuer_name, bond_maturity_date,bond_currency,face_value) VALUES ();
+INSERT INTO security (security_id,cusip,isin,issuer_name, bond_maturity_date,bond_currency,face_value) VALUES ();
+
+INSERT INTO trade (trade_id,book_id,security_id,counter_party_id,bond_holder,trade_type,trade_currency,quantity,trade_settlement_date,trade_status,trade_date,unit_price,coupon_percent,status_of,type_of) VALUES ();
+INSERT INTO trade (trade_id,book_id,security_id,counter_party_id,bond_holder,trade_type,trade_currency,quantity,trade_settlement_date,trade_status,trade_date,unit_price,coupon_percent,status_of,type_of) VALUES ();
+INSERT INTO trade (trade_id,book_id,security_id,counter_party_id,bond_holder,trade_type,trade_currency,quantity,trade_settlement_date,trade_status,trade_date,unit_price,coupon_percent,status_of,type_of) VALUES ();
+INSERT INTO trade (trade_id,book_id,security_id,counter_party_id,bond_holder,trade_type,trade_currency,quantity,trade_settlement_date,trade_status,trade_date,unit_price,coupon_percent,status_of,type_of) VALUES ();
+INSERT INTO trade (trade_id,book_id,security_id,counter_party_id,bond_holder,trade_type,trade_currency,quantity,trade_settlement_date,trade_status,trade_date,unit_price,coupon_percent,status_of,type_of) VALUES ();
+INSERT INTO trade (trade_id,book_id,security_id,counter_party_id,bond_holder,trade_type,trade_currency,quantity,trade_settlement_date,trade_status,trade_date,unit_price,coupon_percent,status_of,type_of) VALUES ();
+INSERT INTO trade (trade_id,book_id,security_id,counter_party_id,bond_holder,trade_type,trade_currency,quantity,trade_settlement_date,trade_status,trade_date,unit_price,coupon_percent,status_of,type_of) VALUES ();
 
 
-INSERT INTO trade (trade_type, trade_currency, quantity, trade_settlement_date, trade_status, trade_date, unit_price, coupon_percent, bond_currency, cusip, face_value, isin, issuer_name, bond_maturity_date, status_of, type_of, book_name, bond_holder) VALUES ('buy', 'USD', 50, '2021-04-08', 'open', '2022-01-05', 90, 4.37, 'USD', '', 1000, 'XS1988387210', 'BNPParibasIssu 4,37% Microsoft Corp (USD)', '2021-05-08', 'active', 'CORP', 'trading_book_1', 'AZ Holdings Inc');
