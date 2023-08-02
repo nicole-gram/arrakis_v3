519b8cf812a487ea943a02ec67aa299b49a2478b
Ian Boyles <ianboyles001@gmail.com>
2023-08-02 20:51:51 +0100

    switched order of tables in temp sql

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -16,4 +16,5 @@ INSERT INTO security ( cusip, isin, issuer_name, bond_maturity_date, bond_curren
 INSERT INTO security ( cusip, isin, issuer_name, bond_maturity_date, bond_currency, face_value) VALUES ( '123456780', 'USN0280EAR64', 'Airbus 3.15% USD', '2021-07-30', 'USD', 900);
 INSERT INTO security ( cusip, isin, issuer_name, bond_maturity_date, bond_currency, face_value) VALUES ('123456bh0', 'A12356111', 'UBS Facebook (USD)', '2021-09-30', 'USD', 900);
 
+
 INSERT INTO trade (book_id, security_id, counter_party_id, bond_holder, trade_type, trade_currency, quantity, trade_settlement_date, trade_status, trade_date, unit_price, coupon_percent, status_of, type_of) VALUES (1, 1, 1, 'AZ Holdings Inc', 'BUY', 'USD', 50, '2021-08-04', 'open', '2021-05-13', 90, 4.37, 'active', 'CORP');
--- java-api/src/main/resources/temp_swethav.sql
+++ java-api/src/main/resources/temp_swethav.sql
@@ -1,3 +1,61 @@
+INSERT IGNORE INTO security (cusip, isin, issuer_name, bond_maturity_date, bond_currency, face_value)
+VALUES
+    -- Row 1
+    ('', 'XS1988387210', 'BNPParibasIssu 4,37% Microsoft Corp (USD)', '2021-08-05', 'USD', 1000),
+    -- Row 2
+    ('', 'XS1988387210', 'BNPParibasIssu 4,37% Microsoft Corp (USD)', '2021-08-05', 'USD', 1000),
+    -- Row 3
+    ('123456780', 'USN0280EAR64', 'Airbus 3.15% USD', '2021-07-30', 'USD', 900),
+    -- Row 4
+    ('123456780', 'USN0280EAR64', 'Airbus 3.15% USD', '2021-07-30', 'USD', 900),
+    -- Row 5
+    ('123456bh0', 'A12356111', 'UBS Facebook (USD)', '2021-09-30', 'USD', 900),
+    -- Row 6
+    ('123456780', 'USN0280EAR64', 'Airbus 3.15% USD', '2021-07-30', 'USD', 900),
+    -- Row 7
+    ('123456bh0', 'A12356111', 'UBS Facebook (USD)', '2021-09-30', 'USD', 900),
+    -- Row 8
+    ('AMZN 3.15 08/22/27 REGS', 'USU02320AG12', 'Amazon', '2021-08-03', 'USD', 900),
+    -- Row 9
+    ('AMZN 3.15 08/22/27 REGS', 'USU02320AG12', 'Amazon', '2021-08-03', 'USD', 900),
+    -- Row 10
+    ('BDCHBW8', 'GB00B6460505', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
+    -- Row 11
+    ('BDCHBW8', 'GB00B6460506', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
+    -- Row 12
+    ('BDCHBW8', 'GB00B6460507', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
+    -- Row 13
+    ('BDCHBW8', 'GB00B6460508', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
+    -- Row 14
+    ('BDCHBW8', 'GB00B6460509', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
+    -- Row 15
+    ('BDCHBW8', 'GB00B6460510', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
+    -- Row 16
+    ('BDCHBW8', 'GB00B6460511', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
+    -- Row 17
+    ('BDCHBW8', 'GB00B6460512', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
+    -- Row 18
+    ('BDCHBW8', 'GB00B6460513', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
+    -- Row 19
+    ('BDCHBW8', 'GB00B6460514', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
+    -- Row 20
+    ('BDCHBW8', 'GB00B6460515', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
+    -- Row 21
+    ('US87973RAA86', 'TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S', '2030-12-22', 'USD', 340),
+    -- Row 22
+    ('US87973RAA86', 'TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S', '2030-12-22', 'USD', 340),
+    -- Row 23
+    ('US87973RAA86', 'TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S', '2030-12-22', 'USD', 340),
+    -- Row 24
+    ('US87973RAA86', 'TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S', '2030-12-22', 'USD', 340),
+    -- Row 25
+    ('IE00B29LNP31', 'First Norway Alpha Kl.IV', '2030-12-22', 'USD', 340),
+    -- Row 26
+    ('IE00B29LNP31', 'First Norway Alpha Kl.IV', '2030-12-22', 'USD', 340),
+    -- Row 27
+    ('IE00B29LNP31', 'First Norway Alpha Kl.IV', '2030-12-22', 'USD', 340),
+    -- Row 28
+    ('IE00B29LNP31', 'First Norway Alpha Kl.IV', '2030-12-22', 'USD', 340);
 INSERT INTO trade (bond_holder, trade_type, trade_currency, quantity, trade_settlement_date, trade_status, trade_date, unit_price, coupon_percent, bond_currency, cusip, face_value, isin, issuer_name, bond_maturity_date, status, type, book_name)
 VALUES
     -- Row 1
@@ -58,61 +116,3 @@ VALUES
     ('Zurich Pension fund 4', 'buy', 'USD', 50, '2021-08-23', 'open', '2012-08-23', 100.13, 2.02, 'USD', '87973RAA8', 690, 'US87973RAA86', 'TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S', '2021-08-06', 'active', 'SOVN', 'Trading_book_4');
 
 
-INSERT IGNORE INTO security (cusip, isin, issuer_name, bond_maturity_date, bond_currency, face_value)
-VALUES
-    -- Row 1
-    ('', 'XS1988387210', 'BNPParibasIssu 4,37% Microsoft Corp (USD)', '2021-08-05', 'USD', 1000),
-    -- Row 2
-    ('', 'XS1988387210', 'BNPParibasIssu 4,37% Microsoft Corp (USD)', '2021-08-05', 'USD', 1000),
-    -- Row 3
-    ('123456780', 'USN0280EAR64', 'Airbus 3.15% USD', '2021-07-30', 'USD', 900),
-    -- Row 4
-    ('123456780', 'USN0280EAR64', 'Airbus 3.15% USD', '2021-07-30', 'USD', 900),
-    -- Row 5
-    ('123456bh0', 'A12356111', 'UBS Facebook (USD)', '2021-09-30', 'USD', 900),
-    -- Row 6
-    ('123456780', 'USN0280EAR64', 'Airbus 3.15% USD', '2021-07-30', 'USD', 900),
-    -- Row 7
-    ('123456bh0', 'A12356111', 'UBS Facebook (USD)', '2021-09-30', 'USD', 900),
-    -- Row 8
-    ('AMZN 3.15 08/22/27 REGS', 'USU02320AG12', 'Amazon', '2021-08-03', 'USD', 900),
-    -- Row 9
-    ('AMZN 3.15 08/22/27 REGS', 'USU02320AG12', 'Amazon', '2021-08-03', 'USD', 900),
-    -- Row 10
-    ('BDCHBW8', 'GB00B6460505', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
-    -- Row 11
-    ('BDCHBW8', 'GB00B6460506', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
-    -- Row 12
-    ('BDCHBW8', 'GB00B6460507', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
-    -- Row 13
-    ('BDCHBW8', 'GB00B6460508', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
-    -- Row 14
-    ('BDCHBW8', 'GB00B6460509', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
-    -- Row 15
-    ('BDCHBW8', 'GB00B6460510', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
-    -- Row 16
-    ('BDCHBW8', 'GB00B6460511', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
-    -- Row 17
-    ('BDCHBW8', 'GB00B6460512', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
-    -- Row 18
-    ('BDCHBW8', 'GB00B6460513', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
-    -- Row 19
-    ('BDCHBW8', 'GB00B6460514', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
-    -- Row 20
-    ('BDCHBW8', 'GB00B6460515', 'HM Treasury United Kingdom', '2021-08-09', 'GBP', 900),
-    -- Row 21
-    ('US87973RAA86', 'TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S', '2030-12-22', 'USD', 340),
-    -- Row 22
-    ('US87973RAA86', 'TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S', '2030-12-22', 'USD', 340),
-    -- Row 23
-    ('US87973RAA86', 'TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S', '2030-12-22', 'USD', 340),
-    -- Row 24
-    ('US87973RAA86', 'TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S', '2030-12-22', 'USD', 340),
-    -- Row 25
-    ('IE00B29LNP31', 'First Norway Alpha Kl.IV', '2030-12-22', 'USD', 340),
-    -- Row 26
-    ('IE00B29LNP31', 'First Norway Alpha Kl.IV', '2030-12-22', 'USD', 340),
-    -- Row 27
-    ('IE00B29LNP31', 'First Norway Alpha Kl.IV', '2030-12-22', 'USD', 340),
-    -- Row 28
-    ('IE00B29LNP31', 'First Norway Alpha Kl.IV', '2030-12-22', 'USD', 340);
