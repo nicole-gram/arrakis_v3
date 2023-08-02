241fefff31765264a0e9687c3af7b166ff424629
Ian Boyles <ianboyles001@gmail.com>
2023-08-02 18:48:00 +0100

    working on inserts for data.sql

--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,19 +1,37 @@
+drop schema if exists bonds;
+CREATE schema bonds;
+USE bonds;
+
 CREATE TABLE book (
     book_id INT PRIMARY KEY,
-    book_name VARCHAR(255),
+    book_name VARCHAR(255)
 );
 
-CREATE TABLE user (
-    user_id INT PRIMARY KEY,
-    user_name VARCHAR(50)
+CREATE TABLE users (
+    u_id INT PRIMARY KEY,
+    u_name VARCHAR(50)
+);
+
+CREATE TABLE counter_party (
+    counter_party_id INT PRIMARY KEY
+);
+
+CREATE TABLE security (
+    security_id INT PRIMARY KEY,
+    cusip VARCHAR(50),
+    isin VARCHAR(50),
+    issuer_name VARCHAR(255),
+    bond_maturity_date DATE,
+    bond_currency VARCHAR(3),
+    face_value INT(10)
 );
 
 CREATE TABLE book_user (
     book_id INT,
-    user_id INT,
-    PRIMARY KEY (book_id, user_id),
+    u_id INT,
+    PRIMARY KEY (book_id, u_id),
     FOREIGN KEY (book_id) REFERENCES book(book_id),
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+    FOREIGN KEY (u_id) REFERENCES users(u_id)
 );
 
 CREATE TABLE trade (
@@ -28,32 +46,13 @@ CREATE TABLE trade (
     trade_settlement_date DATE,
     trade_status VARCHAR(50),
     trade_date DATE,
-    unit_price DECIMAL(18, 2),
-    coupon_percent DECIMAL(5, 2),
-    bond_currency VARCHAR(3),
-    cusip VARCHAR(50),
-    face_value DECIMAL(18, 2),
-    isin VARCHAR(50),
-    issuer_name VARCHAR(255),
-    bond_maturity_date DATE,
-    status VARCHAR(50),
-    type VARCHAR(50),
+    unit_price DECIMAL(6, 3),
+    coupon_percent DECIMAL(5, 4),
+    status_of VARCHAR(50),
+    type_of VARCHAR(50),
     FOREIGN KEY (book_id) REFERENCES book(book_id),
     FOREIGN KEY (security_id) REFERENCES security(security_id),
     FOREIGN KEY (counter_party_id) REFERENCES counter_party(counter_party_id)
-    -- Add other relevant columns for the trade entity
-    -- ...
 );
 
-CREATE TABLE security (
-    security_id INT PRIMARY KEY
-    -- Add other relevant columns for the security entity
-    -- ...
-);
-
-CREATE TABLE counter_party (
-    counter_party_id INT PRIMARY KEY
-    -- Add other relevant columns for the counter_party entity
-    -- ...
-);
 
