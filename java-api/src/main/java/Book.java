f92128780e0dfd3964d60cdfdd6b9570d60b25e1
nicole-gram <nicolegramenides@gmail.com>
2023-08-03 18:22:33 +0100

    rough draft of mvp 1 rows 1-2

--- java-api/src/main/java/Book.java
+++ java-api/src/main/java/Book.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Book {
+import java.time.LocalDate;
+public class Book {
+    //this represents each bond with its properties (1 and 2)
+    private String name;
+    private LocalDate maturityDate;
+    public Book(String name, LocalDate maturityDate) {
+        this.name = name;
+        this.maturityDate = maturityDate;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getMaturityDate() {
+        return maturityDate;
+    }
 }
--- java-api/src/main/java/Main.java
+++ java-api/src/main/java/Main.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Main {
+import java.time.LocalDate;
+import java.util.List;
+public class Main {
+    public static void main(String[] args) {
+        //utilizes the Book and Portfolio to simulate the login and display the active bonds
+
+        //Simulate login (idk if that actually will) and create the user's portfolio
+        Portfolio userPortfolio = new Portfolio();
+
+        //Adding the bonds to the user's portfolio
+        userPortfolio.addBond(new Book("Bond 1"));
+        userPortfolio.addBond(new Book("Bond 2"));
+        userPortfolio.addBond(new Book("Bond 3"));
+
+        //Create bonds with maturity dates
+        LocalDate today = LocalDate.now();
+        Book bond1 = new Book("Bond 1", today.minusDays(6));
+        Book bond2 = new Book("Bond 2", today.minusDays(5));
+        Book bond3 = new Book("Bond 3", today);
+        Book bond4 = new Book("Bond 4", today.minusDays(5));
+        Book bond5 = new Book("Bond 5", today.minusDays(6));
+
+        //Add these bonds ^^ to the portfolio
+        userPortfolio.addBond(bond1);
+        userPortfolio.addBond(bond2);
+        userPortfolio.addBond(bond3);
+        userPortfolio.addBond(bond4);
+        userPortfolio.addBond(bond5);
+
+        //Display bonds due for maturity within last and next 5 days
+        List<Book> dueBonds = userPortfolio.getBondsDueForMaturity();
+        System.out.println("Bonds Due for Maturity within Last and Next 5 Days");
+        for (Book bond: dueBonds) {
+            System.out.println(bond.getName());
+        }
+
+        //Display the active bonds for the logged-in user
+        userPortfolio.displayBonds();
+    }
+
+
+
 }
